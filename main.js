df5d6fc013208d9556af7ec0e1ee7b924f31bbfa
Carlos Eduardo <eduvortexx@icloud.com>
2026-02-12 20:08:05 -0300

    alteração na estrutura

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let trilho = document.getElementById("trilho")
-let body = document.querySelector("body")
+const trilho = document.getElementById("trilho");
+const body = document.body;
 
-trilho.addEventListener("click", ()=>{
-    trilho.classList.toggle("dark")
-    body.classList.toggle("dark")
-})
+trilho.addEventListener("click", () => {
+    trilho.classList.toggle("dark");
+    body.classList.toggle("dark");
+});
